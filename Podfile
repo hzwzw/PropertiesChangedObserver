8b9294b74ef3abc8e3f695f9988e42c1a6e27362
wangzhiwei <hzwzw1@112233.local>
2015-06-27 16:42:43 -0700

    first commit

---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :ios, '8.0'
+use_frameworks!
+pod 'KVOController', :git => 'git@github.com:facebook/KVOController.git'
---
+++ Pods/Target Support Files/Pods-KVOController/Pods-KVOController-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "FBKVOController.h"
+#import "NSObject+FBKVOController.h"
+
+FOUNDATION_EXPORT double KVOControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char KVOControllerVersionString[];
+
